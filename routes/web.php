7be46e361cfa08046f9d7c50739805dd1d9d85f9
Martin Wilches <martindotdev@gmail.com>
2024-08-05 14:44:31 -0500

    Enrutamiento basico

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,19 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+/**
+ * Todas las rutas se pueden definir utilizando un controlador de recursos, siguiendo una estructura condicional 
+ * - index :: La ruta se utiliza para mostrar el formulario y una lista
+ * - store :: La ruta se utiliza para guardar nuevos recursos
+*/
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    /**
+     * Las rutas anteriores utilizan 2 middleware 
+     * - auth :: Garantiza que solo los usuarios registrados puedan acceder a la ruta
+     * - verified :: Se utiliza para habilitar la verificacion de correo electronico
+    */
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
